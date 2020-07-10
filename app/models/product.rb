7ff7500adebe9d454cf95f57c405bc2bf173a562
Ayush <aayushj.pis@gmail.com>
2020-07-10 21:56:05 +0530

    added validation to model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   has_many :variations, inverse_of: :product
   accepts_nested_attributes_for :variations
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
--- app/models/variation.rb
+++ app/models/variation.rb
@@ -4,4 +4,8 @@ class Variation < ApplicationRecord
   has_many :option_values, through: :option_value_variations
 
   accepts_nested_attributes_for :option_value_variations
+
+  validates :sku, presence: true, uniqueness: true
+  validates :quantity, presence: true, numericality: true
+  validates :price, presence: true, numericality: true
 end
